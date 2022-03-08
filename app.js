04a9443f458947e887f11fd186397b17a73633ad
Geunmo Kim <kimgeunmo@gmail.com>
2022-03-08 22:00:25 +0100

    2.2 Variables

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
+const a = 5;
+const b = 2;
+
+console.log(`${a} is ${b}`);
+
 /* 2.1 Basic Data Types
 Integer: basic numbers; 1 ,13, 10000;
 Float: numbers with decimal point; 1.523, 0.232;
 String: texts within quotation marks(""); "hello world"
+*/
+
+/* 2.2 Variables
+console.log(argument) is the basic print function for console;
+JS reads your code from top to bottom;
+Variable in programming is used to save or hold a value;
+When you need to repeatedly use same value or need to change
+certain value that is used in other parts of your code,
+it is better to save that value as a variable;
+In order to create a variable, code as below:
+const a = 5;
+Now whenever you use your variable (a) in your code, it will bring the value (5);
+If your variable name is longer than a single word,
+convention is to use camelCaseSystem in JS;
 */
