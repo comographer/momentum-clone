270b41794021192dbecf71799fdbcd43a057fef2
Geunmo Kim <kimgeunmo@gmail.com>
2022-03-14 21:59:44 +0100

    3.5 More Events

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ querySelectorAll will select all elements that match the query;
 within querySelector, you can use CSS Selectors such as nth-child, etc;
 */
 
-/* 3.3 Events
+/* 3.3 - 3.5 Events
 On JS, what we will do the most is listening to events;
 Events are everything that can happen in the browser;
 connecting to wifi, mouse is on top of h1, mouse leaves the windows, etc;
@@ -34,20 +34,37 @@ This funciton takes two arguments: "event" & "handlerFunction";
 
 "event" : the event we are trying to catch;
 "handlerFunction" : function that will be executed when the "event" occurs;
+
+You can find all the events we can use on MDN;
+We can add multiple events on a single element;
+
+Just like "document", JS has "window" by default;
+Using "window", we can detect events such as "resize", "copy" etc;
+From "window", we can also check status of the internet;
 */
 
-const title = document.querySelector(".hello");
+const h1 = document.querySelector(".hello");
 
-const handleTitleClick = () => (title.style.color = "blue");
+const handleTitleClick = () => (h1.style.color = "blue");
 
-const handleMouseEnter = () => (title.innerText = "Mouse is here");
+const handleMouseEnter = () => (h1.innerText = "Mouse is here");
 
-const handleMouseLeave = () => (title.innerText = "Mouse is Gone");
+const handleMouseLeave = () => (h1.innerText = "Mouse is Gone");
 
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
 
-/* 3.4 Events part Two
-We can find 
-*/
+const handleWindowResize = () =>
+  (document.body.style.backgroundColor = "tomato");
+
+const handleWindowCopy = () => alert("Copied");
+
+const handleWindowOffline = () => alert("SOS No WiFi");
+
+const handleWindowOnline = () => alert("Good to Go");
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
