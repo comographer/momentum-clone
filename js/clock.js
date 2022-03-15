25d66951cbc2366d8569ec936101336aa14dd0fa
Geunmo Kim <kimgeunmo@gmail.com>
2022-03-15 23:17:57 +0100

    5.0 Intervals

---
+++ js/clock.js
@@ -0,0 +1,8 @@
+const clock = document.querySelector(".clock");
+
+/* 5.0 Intervals
+Interval is something that happens per every time unit;
+In JS, we can set interval for functions we want to execute over and over;
+To set interval: setInterval(function, interval);
+The interval in setInterval should be written in miliseconds;
+*/
