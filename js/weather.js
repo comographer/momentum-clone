c8ca200aea2ed357787ae94d641e82d7df86e78e
Geunmo Kim <kimgeunmo@gmail.com>
2022-03-17 22:18:51 +0100

    8.1 Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,18 @@
+const weather = document.querySelector(".weather span:nth-child(1)");
+const city = document.querySelector(".weather span:nth-child(2)");
+
+const API_KEY = "396cdae68a7936f2d7a525a9e8f5e27f";
+
 const onGeoSuccess = (position) => {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log(`You live in ${lat}, ${lng}`);
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      city.innerText = data.name;
+      weather.innerText = `${Math.round(data.main.temp)} ℃`;
+    });
 };
 
 const onGeoError = () => {
@@ -19,4 +30,13 @@ Second, function to call when there is an error;
 
 For the successCallbackFunction, JS provides the user's position as an argument;
 From that position object, we can get the latitude and the longitude;
+*/
+
+/* 8.1 Weather API
+API is how programs & apps communicate with each other;
+With the langitude and longitude, we can use openweathermap.org API
+to get City Name and temperature data;
+When we have the API url, we can get data from the API url using fetch(url);
+Since fetch() is a promise, meaning it can take quite some time depending what we are fetching,
+we add then() to the fetch: fetch(url).then(response => response.json())
 */
